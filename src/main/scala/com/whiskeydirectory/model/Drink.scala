759dce9e609bc626fd617c57cfcc51c67dfd4dab
Bryan J Swift <bryan.j.swift@gmail.com>
2009-04-17 12:55:24 -0400

    fixed a stupid error and added Drink's backing object

--- src/main/scala/com/whiskeydirectory/model/Drink.scala
+++ src/main/scala/com/whiskeydirectory/model/Drink.scala
@@ -5,12 +5,13 @@ class Drink(val name:String, val tags:Map[String,Int], val location:Option[Locat
 	def this(name:String, tags:Map[String,Int]) = this(name, tags, None)
 
 	def add(tag:String) {
-		val ntags = if (tags contains tag)
-				tags ++ Map(tag -> (tags(tag) + 1))
-			else
-				tags ++ Map(tag -> 1)
-		new Drink(name, ntags, location)
+		val ntags = if (tags contains tag) tags ++ Map(tag -> (tags(tag) + 1)) else tags ++ Map(tag -> 1)
+		Drink(name, ntags, location)
 	}
 
-	def change(location:Location) = new Drink(name, ntags, location)
+	def change(location:Location) = Drink(name, tags, Some(location))
+}
+
+object Drink {
+	def apply(name:String, tags:Map[String,Int], location:Option[Location]) = new Drink(name, tags, location)
 }
