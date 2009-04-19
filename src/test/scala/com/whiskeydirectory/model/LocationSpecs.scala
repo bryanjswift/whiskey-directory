f4dff4896079b3a60ea2cc73329cebb33eae3276
Bryan J Swift <bryan.j.swift@gmail.com>
2009-04-19 01:31:23 -0400

    updated LocationSpecs

--- src/test/scala/com/whiskeydirectory/model/LocationSpecs.scala
+++ src/test/scala/com/whiskeydirectory/model/LocationSpecs.scala
@@ -6,7 +6,7 @@ import org.specs.runner.JUnit4
 class LocationTest extends JUnit4(LocationSpecs)
 
 object LocationSpecs extends Specification {
-	"CrossStreet must have at least 2 streets" in {
+	"CrossStreet must have at least 2 Streets" in {
 		try {
 			new CrossStreet(List())
 			false
@@ -15,7 +15,7 @@ object LocationSpecs extends Specification {
 				true // expected
 		}
 	}
-	"CrossStreet must have fewer than 4 streets" in {
+	"CrossStreet must have fewer than 4 Streets" in {
 		try {
 			new CrossStreet(List(new NorthSouthStreet("test 1"), new NorthSouthStreet("test 2"),
 				new EastWestStreet("test 3"), new EastWestStreet("test 4")))
@@ -24,5 +24,27 @@ object LocationSpecs extends Specification {
 			case iae:IllegalArgumentException =>
 				true // expected
 		}
+	}
+	"CrossStreet may not have all NorthSouthStreets" in {
+		try {
+			new CrossStreet(List(new NorthSouthStreet("test 1"), new NorthSouthStreet("test 2"), new NorthSouthStreet("test 3")))
+			false
+		} catch {
+			case iae:IllegalArgumentException =>
+				true // expected
+		}
+	}
+	"CrossStreet may not have all EastWestStreets" in {
+		try {
+			new CrossStreet(List(new EastWestStreet("test 1"), new EastWestStreet("test 2"), new EastWestStreet("test 3")))
+			false
+		} catch {
+			case iae:IllegalArgumentException =>
+				true // expected
+		}
+	}
+	"CrossStreet may have two or three Streets" in {
+		new CrossStreet(List(new NorthSouthStreet("test 1"), new EastWestStreet("test 2")))
+		new CrossStreet(List(new NorthSouthStreet("test 1"), new EastWestStreet("test 2"), new NorthSouthStreet("test 3")))
 	}
 }
