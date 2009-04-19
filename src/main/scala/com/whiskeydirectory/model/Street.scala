7ac836e74f19d32575fe8a6eae7acc08c9265108
Bryan J Swift <bryan.j.swift@gmail.com>
2009-04-18 23:50:22 -0400

    changed Street to require longitudinal or latitudinal to be true

--- src/main/scala/com/whiskeydirectory/model/Street.scala
+++ src/main/scala/com/whiskeydirectory/model/Street.scala
@@ -2,5 +2,5 @@ package com.whiskeydirectory.model
 
 class Street(val name:String, val longitudinal:Boolean, val latitudinal:Boolean) {
 	require(name != null && name != "")
-	require(!(longitudinal && latitudinal))
+	require(!(longitudinal && latitudinal) && (longitudinal || latitudinal))
 }
