0be5d0252bca37a4abe805f0bab93c41b4d6229d
Bryan J Swift <bryan.j.swift@gmail.com>
2009-04-18 23:36:17 -0400

    updated the Drink backing object's apply method to handle the case of null parameters

--- src/main/scala/com/whiskeydirectory/model/Drink.scala
+++ src/main/scala/com/whiskeydirectory/model/Drink.scala
@@ -1,10 +1,20 @@
 package com.whiskeydirectory.model
 
-class Drink(val name:String, val tags:Option[List[String]], val location:Option[Location]) {
-	def this(name:String) = this(name, Map(), None)
-	def this(name:String, tags:Map[String,Int]) = this(name, tags, None)
+class Drink(val name:String, val tags:Option[List[String]], val establishment:Option[Establishment]) {
+	require(name != null && name.length > 0)
+	def this(name:String) = this(name, None, None)
+	def this(name:String, tags:List[String]) = this(name, Some(tags), None)
 }
 
 object Drink {
-	def apply(name:String, tags:Option[List[String]], location:Option[Location]) = new Drink(name, tags, location)
+	def apply(name:String, tags:List[String], establishment:Establishment) {
+		if (tags == null && establishment == null)
+			new Drink(name, None, None)
+		else if (tags == null)
+			new Drink(name, None, Some(establishment))
+		else if (establishment == null)
+			new Drink(name, Some(tags), None)
+		else
+			new Drink(name, Some(tags), Some(establishment))
+	}
 }
