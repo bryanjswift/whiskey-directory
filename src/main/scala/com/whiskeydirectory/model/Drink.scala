7733845cf50d8a2133ed21e2218cbb0cba3c8f49
Bryan J Swift <bryan.j.swift@gmail.com>
2009-04-18 22:28:04 -0400

    removed some mutability from Drink class

--- src/main/scala/com/whiskeydirectory/model/Drink.scala
+++ src/main/scala/com/whiskeydirectory/model/Drink.scala
@@ -1,18 +1,10 @@
 package com.whiskeydirectory.model
 
-class Drink(val name:String, val tags:Map[String,Int], val location:Option[Location]) {
+class Drink(val name:String, val tags:Option[List[String]], val location:Option[Location]) {
 	def this(name:String) = this(name, Map(), None)
 	def this(name:String, tags:Map[String,Int]) = this(name, tags, None)
-
-	def add(tag:String) {
-		val ntags = if (tags contains tag) tags ++ Map(tag -> (tags(tag) + 1)) else tags ++ Map(tag -> 1)
-		Drink(name, ntags, location)
-	}
-	def apply(tag:String) = this add tag
-	def apply(location:Location) = this change location
-	def change(location:Location) = Drink(name, tags, Some(location))
 }
 
 object Drink {
-	def apply(name:String, tags:Map[String,Int], location:Option[Location]) = new Drink(name, tags, location)
+	def apply(name:String, tags:Option[List[String]], location:Option[Location]) = new Drink(name, tags, location)
 }
