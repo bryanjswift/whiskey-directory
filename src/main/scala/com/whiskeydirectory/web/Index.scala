09cb1d13771f8747ad1ef743a6685ebcebad2373
Bryan J Swift <bryan.j.swift@gmail.com>
2009-04-23 22:34:25 -0400

    trying to get a servlet to work

---
+++ src/main/scala/com/whiskeydirectory/web/Index.scala
@@ -0,0 +1,10 @@
+package com.whiskeydirectory.web
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest => Request, HttpServletResponse => Response}
+
+class Index extends HttpServlet {
+	override def doGet(request:Request, response:Response) {
+		response.setContentType("text/plain")
+		response.getWriter.println("Hello, world")
+	}
+}
