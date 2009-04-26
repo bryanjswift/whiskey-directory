f07a157440cd04daf2f6f3bf29d035d58de2ed38
Bryan J Swift <bryan.j.swift@gmail.com>
2009-04-25 20:04:35 -0400

    added definition for what I think I want Street to look like, but commented it out because JDO annotations and Scala classes aren't playing nice

--- src/main/scala/com/whiskeydirectory/model/Street.scala
+++ src/main/scala/com/whiskeydirectory/model/Street.scala
@@ -1,11 +1,18 @@
 package com.whiskeydirectory.model
 
 import com.whiskeydirectory.persistence.{Persistable,Savable}
+import javax.jdo.annotations._
 
 abstract class Street(val name:String) {
 	require(name != null && name != "")
 	def longitudinal:Boolean
 	def latitudinal:Boolean
+
+/*
+	def getPersistable():PersistableStreet = {
+		new PersistableStreet(name,longitudinal,latitudinal)
+	}
+*/
 
 	def getPersistable():PersistableStreet = {
 		val street = new PersistableStreet()
@@ -25,3 +32,16 @@ case class EastWestStreet(override val name:String) extends Street(name) {
 	override val longitudinal = false
 	override val latitudinal = true
 }
+
+/*
+@PersistenceCapable { val identityType = IdentityType.APPLICATION }
+class PersistableStreet(
+		@Persistent override val name:String,
+		@Persistent override val longitudinal:Boolean,
+		@Persistent override val latitudinal:Boolean) extends Street(name) {
+	def this() = this("", false, false)
+	@PrimaryKey
+	@Persistent { val valueStrategy = IdGeneratorStrategy.IDENTITY }
+	var id:Long = 0
+}
+*/
