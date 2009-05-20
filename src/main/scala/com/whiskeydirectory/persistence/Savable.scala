bbbf59f30dcf4f4eb90aaa792027c3ab3a010eee
Bryan J Swift <bryan.j.swift@gmail.com>
2009-05-20 19:05:00 -0400

    changed id from an Int to a Long

--- src/main/scala/com/whiskeydirectory/persistence/Savable.scala
+++ src/main/scala/com/whiskeydirectory/persistence/Savable.scala
@@ -2,7 +2,7 @@ package com.whiskeydirectory.persistence
 
 // these methods could probably be implemented using reflection
 trait Savable {
-	var id:Int = _
+	var id:Long = _
 	def insert():String
 	def statement():String
 	def update():String
