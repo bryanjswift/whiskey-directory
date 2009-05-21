b25ce408644c26c17d3e533cd45e26e186908f7e
Bryan J Swift <bryan.j.swift@gmail.com>
2009-05-20 22:58:11 -0400

    made the Street abstract class a sealed class

--- src/main/scala/com/whiskeydirectory/model/Street.scala
+++ src/main/scala/com/whiskeydirectory/model/Street.scala
@@ -2,7 +2,7 @@ package com.whiskeydirectory.model
 
 import com.whiskeydirectory.persistence.{Savable,Tabled}
 
-abstract class Street(val name:String) extends Savable {
+sealed abstract class Street(val name:String) extends Savable {
 	require(name != null && name != "")
 	def longitudinal:Boolean
 	def latitudinal:Boolean
