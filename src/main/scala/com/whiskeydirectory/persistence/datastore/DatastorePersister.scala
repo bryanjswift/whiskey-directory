cab94c3b3329b627692861658e48e2c744ae26fc
Bryan J Swift <bryan.j.swift@gmail.com>
2009-06-06 13:19:10 -0400

    updated DatastorePersister to use the class' simple name for the table

--- src/main/scala/com/whiskeydirectory/persistence/datastore/DatastorePersister.scala
+++ src/main/scala/com/whiskeydirectory/persistence/datastore/DatastorePersister.scala
@@ -7,7 +7,7 @@ import java.io.Serializable
 object DatastorePersister extends Persister {
 	val datastore = DatastoreServiceFactory.getDatastoreService()
 	def save(obj:Savable):Serializable = {
-		val entity = new Entity(obj.table,obj.id.toString)
+		val entity = new Entity(obj.getClass().getSimpleName(),obj.id.toString)
 		val map = obj.fields zip obj.values
 		map.foreach(t => {
 			entity.setProperty(t._1,t._2)
