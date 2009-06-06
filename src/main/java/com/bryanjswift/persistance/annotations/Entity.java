42200c2672ee5ea11a9ec348c30f1e9a1c483d8f
Bryan J Swift <bryan.j.swift@gmail.com>
2009-06-06 16:08:34 -0400

    added some persistence annotations

---
+++ src/main/java/com/bryanjswift/persistance/annotations/Entity.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.bryanjswift.persistance.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author bryanjswift
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Entity {
+	String type();
+}
---
+++ src/main/java/com/bryanjswift/persistance/annotations/Persist.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.bryanjswift.persistance.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author bryanjswift
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Persist {
+
+}
